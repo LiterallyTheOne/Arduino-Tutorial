90a728fb1307e3af916d4ba7f034850a100dc355
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-10-01 14:05:37 +0330

    Add the code from read serial until

---
+++ src/4-serial-analog-read/a4_3_read_serial_until.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    String result = Serial.readStringUntil('\n');
+    Serial.println(result);
+  }
+}
