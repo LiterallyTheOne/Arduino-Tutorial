c848a79f9dc5e6fe0552e4bd75f0c7da68ee1c7b
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-11-16 13:50:19 +0330

    Change stop to pause

--- src/6-interrupt/a6_2_interrupt_pause.cpp
+++ src/6-interrupt/a6_2_interrupt_pause.cpp
@@ -5,7 +5,7 @@ int led_pins[8] = {6, 7, 8, 9, 10, 11, 12, 13};
 int current_led = 0;
 int x = 1;
 
-void isr_stop()
+void isr_pause()
 {
   x = 1 - x;
 }
@@ -17,7 +17,7 @@ void setup()
     pinMode(led_pins[i], OUTPUT);
   }
 
-  attachInterrupt(digitalPinToInterrupt(2), isr_stop, RISING);
+  attachInterrupt(digitalPinToInterrupt(2), isr_pause, RISING);
 }
 
 void loop()
