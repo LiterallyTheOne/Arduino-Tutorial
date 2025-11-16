6da5a02f0d0aa900d84a798effb2377a5d5b1f33
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-11-16 11:08:56 +0330

    Add a code for leds_counter

---
+++ src/6-interrupt/a6_4_leds_counter.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+uint8_t a = 0;
+
+int x = 1;
+
+int led_pins[8] = {6, 7, 8, 9, 10, 11, 12, 13};
+
+void isr_stop()
+{
+  x = 1 - x;
+}
+
+void isr_reset()
+{
+  a = 0;
+}
+
+void setup()
+{
+  for (int i = 0; i < 8; i++)
+  {
+    pinMode(led_pins[i], OUTPUT);
+  }
+
+  attachInterrupt(digitalPinToInterrupt(2), isr_stop, FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), isr_reset, FALLING);
+}
+
+void loop()
+{
+  for (int i = 0; i < 8; i++)
+  {
+    bool led_on = ((a >> i) & 0x01);
+    digitalWrite(led_pins[i], led_on);
+  }
+
+  a += x;
+  delay(200);
+}
