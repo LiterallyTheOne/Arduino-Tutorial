c3a45cca6ca7ca06c9bd52abfeecd437505e9ddd
Ramin Zarebidoky <ramin.zarebidoky@gmail.com>
2025-11-30 09:34:05 +0330

    Add a7_2_clock_read.cpp simulation

---
+++ src/7-I2C/a7_2_clock_read.cpp
@@ -0,0 +1,41 @@
+#include <Arduino.h>
+#include <Wire.h>
+
+#define DS1307_ADDRESS 0x68
+
+#define TIME_READ 7
+
+byte time[TIME_READ];
+
+void readDateTime()
+{
+  Wire.beginTransmission(DS1307_ADDRESS);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  Wire.requestFrom(DS1307_ADDRESS, 7);
+
+  for (int i = 0; i < TIME_READ; i++)
+  {
+    time[i] = Wire.read();
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+}
+
+void loop()
+{
+  readDateTime();
+
+  for (int i = 0; i < TIME_READ; i++)
+  {
+    Serial.print(String(time[i], HEX) + " ");
+  }
+  Serial.println();
+
+  delay(1000);
+}
